1096315398a7ce13b654f77f3d04e81fd434d628
Irum <irummf@gmail.com>
2020-04-12 02:13:46 +0100

    Router file

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,8 @@
 import React, { Component } from "react";
-import { Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
 
 import "../App.css";
-import { AIMCompanyChart, AIMCompanyIndex, AIMCompanyShow } from "./AIM";
-import { LSECompanyChart, LSECompanyIndex, LSECompanyShow } from "./LSE";
-import Homepage from "./Homepage";
-import history from "../history";
+import CompaniesRouter from "./Router";
 
 class App extends Component {
   constructor(props) {
@@ -19,95 +15,11 @@ class App extends Component {
   }
 
   render() {
-    return <div className="App">this.aimRouter() this.lseRouter()</div>;
-  }
-
-  aimRouter() {
     return (
-      <div>
-        <Router history={history}>
-          <Homepage />
-          <div>
-            <Switch>
-              <Route
-                exact
-                path="/aim/show/:company"
-                render={props => (
-                  <AIMCompanyShow
-                    {...props}
-                    aimCompanies={this.state.aimCompanies}
-                  />
-                )}
-              />
-              <Route
-                exact
-                path="aim/companies/show/chart/:company"
-                render={props => (
-                  <AIMCompanyChart
-                    {...props}
-                    aimCompanies={this.state.aimCompanies}
-                  />
-                )}
-              />
-              <Route
-                path="aim/companies/:sort"
-                render={props => (
-                  <AIMCompanyIndex
-                    {...props}
-                    aimCompanies={this.state.aimCompanies}
-                    searchQuery={this.state.searchQuery}
-                    changeSearchQuery={this.changeSearchQuery}
-                  />
-                )}
-              />
-            </Switch>
-          </div>
-        </Router>
-      </div>
-    );
-  }
-
-  lseRouter() {
-    return (
-      <div>
-        <Router history={history}>
-          <Homepage />
-          <div>
-            <Switch>
-              <Route
-                exact
-                path="/lse/show/:company"
-                render={props => (
-                  <LSECompanyShow
-                    {...props}
-                    lseCompanies={this.state.lseCompanies}
-                  />
-                )}
-              />
-              <Route
-                exact
-                path="lse/companies/show/chart/:company"
-                render={props => (
-                  <LSECompanyChart
-                    {...props}
-                    lseCompanies={this.state.lseCompanies}
-                  />
-                )}
-              />
-              <Route
-                path="lse/companies/:sort"
-                render={props => (
-                  <LSECompanyIndex
-                    {...props}
-                    lseCompanies={this.state.lseCompanies}
-                    searchQuery={this.state.searchQuery}
-                    changeSearchQuery={this.changeSearchQuery}
-                  />
-                )}
-              />
-            </Switch>
-          </div>
-        </Router>
+      <div className="App">
+        {Object.keys(CompaniesRouter).map(router => (
+          <div>{router}</div>
+        ))}
       </div>
     );
   }
---
+++ src/components/Router.js
@@ -0,0 +1,101 @@
+import React from "react";
+import { Router, Switch, Route } from "react-router-dom";
+
+import { AIMCompanyChart, AIMCompanyIndex, AIMCompanyShow } from "./AIM";
+import { LSECompanyChart, LSECompanyIndex, LSECompanyShow } from "./LSE";
+import Homepage from "./Homepage";
+import history from "../history";
+
+const CompaniesRouter = {
+  aimRouter() {
+    return (
+      <div>
+        <Router history={history}>
+          <Homepage />
+          <div>
+            <Switch>
+              <Route
+                exact
+                path="/aim/show/:company"
+                render={props => (
+                  <AIMCompanyShow
+                    {...props}
+                    aimCompanies={this.state.aimCompanies}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path="aim/companies/show/chart/:company"
+                render={props => (
+                  <AIMCompanyChart
+                    {...props}
+                    aimCompanies={this.state.aimCompanies}
+                  />
+                )}
+              />
+              <Route
+                path="aim/companies/:sort"
+                render={props => (
+                  <AIMCompanyIndex
+                    {...props}
+                    aimCompanies={this.state.aimCompanies}
+                    searchQuery={this.state.searchQuery}
+                    changeSearchQuery={this.changeSearchQuery}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  },
+
+  lseRouter() {
+    return (
+      <div>
+        <Router history={history}>
+          <Homepage />
+          <div>
+            <Switch>
+              <Route
+                exact
+                path="/lse/show/:company"
+                render={props => (
+                  <LSECompanyShow
+                    {...props}
+                    lseCompanies={this.state.lseCompanies}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path="lse/companies/show/chart/:company"
+                render={props => (
+                  <LSECompanyChart
+                    {...props}
+                    lseCompanies={this.state.lseCompanies}
+                  />
+                )}
+              />
+              <Route
+                path="lse/companies/:sort"
+                render={props => (
+                  <LSECompanyIndex
+                    {...props}
+                    lseCompanies={this.state.lseCompanies}
+                    searchQuery={this.state.searchQuery}
+                    changeSearchQuery={this.changeSearchQuery}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
+};
+
+export default CompaniesRouter;
